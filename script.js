cb72e93a2a254141252fa09dc573ef8d5481839d
adrianogtl <adrianogtlopes@gmail.com>
2024-06-11 21:14:54 -0300

    code clipboard feature

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-const nav = document.querySelector("nav");
-const docTitle = document.querySelector("#doc-title");
-const scrollToTopBtn = document.querySelector("#scroll-to-top-btn");
-const scrollToTopContainer = document.querySelector(".scroll-to-top-container");
-
-const isPhoneOrTouch =
-  /Android|iPhone/i.test(navigator.userAgent) || navigator.maxTouchPoints > 0;
-
 function animateLogo() {
+  const nav = document.querySelector("nav");
+  const docTitle = document.querySelector("#doc-title");
   nav.onmouseenter = (e) => docTitle.classList.add("nav-hover");
   nav.onmouseout = (e) => {
     const mouseOutNav = !nav.contains(e.relatedTarget);
@@ -16,30 +10,63 @@ function animateLogo() {
   };
 }
 
-function fixBackToTopBtn() {
+function fixBackToTopBtn(scrollToTopBtn, scrollToTopContainer) {
   scrollToTopContainer.style.position = "absolute";
   scrollToTopBtn.style.display = "none";
   scrollToTopBtn.style.position = "fixed";
   scrollToTopBtn.style.right = "unset";
 }
 
-scrollToTopBtn.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
+function addClipboardBtns() {
+  const containerCodeArr = Array.from(
+    document.querySelectorAll(".container-code")
+  );
+
+  containerCodeArr.forEach((containerCode) => {
+    containerCode.innerHTML += `<button class="clipboard-btn" title="Copy to clipboard">Copy</button>`;
+
+    const clipboardBtn = containerCode.children[1];
+    const preTagContent = containerCode.children[0].textContent;
+    addClipboardEvent(clipboardBtn, preTagContent);
   });
-});
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 800) {
-    scrollToTopBtn.style.display = "block";
-  } else {
-    scrollToTopBtn.style.display = "none";
-  }
-});
-
-if (isPhoneOrTouch) {
-  fixBackToTopBtn();
-} else {
-  animateLogo();
 }
+
+function addClipboardEvent(clipboardBtn, preTagContent) {
+  clipboardBtn.onclick = () => {
+    navigator.clipboard.writeText(preTagContent);
+  };
+}
+
+function addScrollEvents(scrollToTopBtn) {
+  scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 800) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  });
+}
+
+function main() {
+  const deviceHasTouchScreen = navigator.maxTouchPoints > 0;
+  const scrollToTopBtn = document.querySelector("#scroll-to-top-btn");
+  const scrollToTopContainer = document.querySelector(
+    ".scroll-to-top-container"
+  );
+
+  addScrollEvents(scrollToTopBtn);
+  if (deviceHasTouchScreen) {
+    fixBackToTopBtn(scrollToTopBtn, scrollToTopContainer);
+  } else {
+    animateLogo();
+    addClipboardBtns();
+  }
+}
+main();
